a2d35a947a08c9d06b46f4ec2c117f5832b20ba2
anuragchillarige <pinganurag@gmail.com>
2024-04-06 14:27:13 -0700

    added functionality to create playlist

--- src/LogIn.jsx
+++ src/LogIn.jsx
@@ -3,7 +3,8 @@ import React, { useEffect } from "react"
 const CLIENT_ID = '05345fe6718845e98a33b228f41eb512'
 const SPOTIFY_AUTH_ENDPT = 'https://accounts.spotify.com/authorize'
 const REDIRECT_URL = 'http://localhost:3000/main'
-const SCOPES = ['playlist-read-private', 'playlist-read-collaborative', 'user-library-read']
+const SCOPES = ['playlist-read-private', 'playlist-read-collaborative', 'user-library-read', 'playlist-modify-public',
+    'playlist-modify-private']
 const SCOPES_URL_PARAM = SCOPES.join("%20")
 
 function LogIn() {
--- src/Main.jsx
+++ src/Main.jsx
@@ -6,6 +6,7 @@ function Main() {
 
     const [token, setToken] = useState("");
     const [data, setData] = useState({})
+    const [userId, setUserId] = useState("")
 
     const getReturnedParams = (hash) => {
         const stringAfterHash = hash.substring(1);
@@ -21,12 +22,11 @@ function Main() {
     }
 
     const handleGetPlaylists = async () => {
-        const res = await fetch("https://api.spotify.com/v1/me/playlists",
-            {
-                headers: {
-                    Authorization: "Bearer " + token,
-                }
+        const res = await fetch("https://api.spotify.com/v1/me/playlists", {
+            headers: {
+                Authorization: "Bearer " + token,
             }
+        }
         ).catch(e => {
             console.error(e)
         })
@@ -34,6 +34,25 @@ function Main() {
         const playlists = await res.json();
         console.log(playlists)
         setData(playlists['items'])
+    }
+
+    const handleCreatePlaylist = async () => {
+        if (userId == '') {
+            return;
+        }
+        await fetch(`https://api.spotify.com/v1/users/${userId}/playlists`, {
+            method: "POST",
+            body: JSON.stringify({
+                name: 'billy bob joe',
+                description: 'created using api',
+                public: false,
+            }),
+            headers: {
+                Authorization: 'Bearer ' + token,
+                "Content-Type": 'application/json'
+
+            }
+        }).catch(e => { console.log(e) })
     }
 
     // for loading the data to local storage
@@ -47,14 +66,31 @@ function Main() {
             localStorage.setItem("accessToken", access_token);
             localStorage.setItem("tokenType", token_type);
             localStorage.setItem("expiresIn", expires_in);
+
+
         }
     }, []);
 
     // for chagning the variables
     useEffect(() => {
-        if (localStorage.getItem("accessToken")) {
-            setToken(localStorage.getItem("accessToken"));
+
+        const fn = async () => {
+            if (localStorage.getItem("accessToken")) {
+                setToken(localStorage.getItem("accessToken"));
+
+                const res = await fetch('https://api.spotify.com/v1/me/', {
+                    headers: {
+                        Authorization: "Bearer " + token,
+                    }
+                })
+                const userInfo = await res.json();
+                console.log(userInfo)
+                setUserId(userInfo.id)
+            }
         }
+
+        fn()
+
     }, [])
 
     return (
@@ -69,6 +105,8 @@ function Main() {
                     ))
                 }
             </div>
+
+            <button onClick={handleCreatePlaylist}>create playlist</button>
 
         </div>
     )
