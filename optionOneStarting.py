e8397450de40fda5d3414d26d4bcb95e1d8c2eef
Sanjay Chandrasekar <sanjay.chandrasekar@gmail.com>
2024-04-06 16:19:47 -0700

    del some files

---
+++ optionOneStarting.py
@@ -0,0 +1,8 @@
+'''
+Starting part for option 1 (user picks b/w 2 options, feeds into model)
+getting 15 genres and starting 2 songs for each genre
+'''
+
+genres = ['Pop', 'Rock', 'Hip Hop', 'R&B/Soul', 'Country',
+    'Electronic/Dance', 'Indie', 'K-Pop', 'Classical', 'Jazz',
+    'Metal', 'Reggae', 'Latin', 'Folk', 'Rap']
