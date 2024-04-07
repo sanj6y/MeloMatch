332cdf2cdea86be46ba7418e7c44a3fc47db46d6
bhavan060 <70976789+bhavan060@users.noreply.github.com>
2024-04-06 19:10:49 -0700

    fixed the home page get started button

--- src/LoadingPage.jsx
+++ src/LoadingPage.jsx
@@ -11,6 +11,20 @@ const logo = require("./listen.png")
 
 
 function LoadingPage() {
+
+  const CLIENT_ID = '05345fe6718845e98a33b228f41eb512'
+  const SPOTIFY_AUTH_ENDPT = 'https://accounts.spotify.com/authorize'
+  const REDIRECT_URL = 'http://localhost:3000/main'
+  const SCOPES = ['playlist-read-private', 'playlist-read-collaborative', 'user-library-read', 'playlist-modify-public',
+      'playlist-modify-private']
+  const SCOPES_URL_PARAM = SCOPES.join("%20")
+
+  const requestAuthorization = () => {
+      console.log("button pressed");
+      window.location = `${SPOTIFY_AUTH_ENDPT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URL}&scope=${SCOPES_URL_PARAM}&response_type=token&show_dialog=true`;
+  }
+
+
   return (
     <div className="landingHolder">
       <NavBar/> 
@@ -18,7 +32,7 @@ function LoadingPage() {
         <div className="welcome-text">
           <p className="dawg1"><TypeAnimation cursor={true} sequence={['Having trouble finding new music?', 1000, 'Tired of listening to the same songs over and over?', 1000]} repeat={Infinity} speed={50}wrapper="span" className="special" /></p>
           <p className="dawg2">MeloMatch is the website for you.</p>
-          <button className="getStarted-button">
+          <button className="getStarted-button" onClick={requestAuthorization}>
             Get Started
           </button>
         </div>
