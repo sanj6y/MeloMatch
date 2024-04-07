0c7ccc9461a65a73c72c6c269af4a1249c9367d3
bhavan060 <70976789+bhavan060@users.noreply.github.com>
2024-04-07 04:21:13 -0700

    Python fixed

--- flaskApp.py
+++ flaskApp.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify, Response
 from flask_cors import CORS, cross_origin
 import spotipy
+import json
 import requests
 from spotipy.oauth2 import SpotifyClientCredentials
 sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id='05345fe6718845e98a33b228f41eb512',
@@ -30,14 +31,30 @@ def song_rec():
     # Logic to receive the string from the frontend
     global playlist
     global ids
-    id = request.get_data()
+    id1 = str(request.get_data(), encoding='utf-8')[1:-1]
     ids = []
-    d = sp.recommendations(seed_tracks=[str(id)], limit=2)
+    print(id1)
+    playlist.append(id1)
+    #d = sp.recommendations(seed_tracks=[str(id)], limit=2)
+    user_headers = {
+    "Authorization": "Bearer " + 'BQCQzwKuIB1sXvhUh19uNPXFQuPv02X8Hx7ojMg7ncPHOdHhRd60UpWcJqbxcMrdZqSkgJpHPN3dZkuqrKjpKtxJAgRVDU6TimBNYlcz6mLYr36bv4WbFuv6W04WfC5e9IPonSjGPY1sYZKNAw7EJGOpNJ7e_GbHsC7LzNX4ybBK4WZ9Vg1_T5St_A',
+    "Content-Type": "application/json"
+    }
 
-    for song in d["tracks"]:
+    user_params = {
+        "limit": 3600
+    }
+
+    
+    
+    d = requests.get("https://api.spotify.com/v1/recommendations?limit=2&seed_tracks={}".format(id1), params=user_params, headers=user_headers)
+    print(d.json())
+
+    for song in d.json()["tracks"]:
         print(song["name"])
-        playlist.append(song["id"])
+        #playlist.append(song["id"])
         ids.append(song['id'])
+    return jsonify(ids)
     
 
 @app.route('/song_get', methods=['GET'])
@@ -49,18 +66,27 @@ def song_get():
 
     return ids
 
+@app.route('/playlist_get', methods=['GET'])
+@cross_origin(origin='localhost',headers=['Content- Type','Authorization'])
+def playlist_get():
+    # Logic to receive the string from the frontend
+    global playlist
+    global ids
+
+    return playlist
+
 @app.route('/genre', methods=['POST'])
 @cross_origin(origin='localhost',headers=['Content- Type','Authorization'])
 def genre():
     # Logic to receive the string from the frontend
     global playlist
     global ids
-    genres = request.get_data()
+    genres = json.loads(request.get_data())
     ids = []
     #d = sp.recommendations(seed_genre=[genres], limit=2)
 
     user_headers = {
-    "Authorization": "Bearer " + 'BQBSs9rMtLPBpVuWAl2ZZXCgnPjVuCYWpB-qHRqQyDFHsymMdhxgpbiNjlhtDr6AgUBLcvmZKKQ0aD7oeEhc-YlTgx-526Fq1dGpPHfrs2t2CQB0yQXhRSPEwz_b-RQapyjAIRZXv0iEIkjetYckYAyFKPZC7aL63XIvQdFF6-p8DgWKykFekLMG-A',
+    "Authorization": "Bearer " + 'BQCQzwKuIB1sXvhUh19uNPXFQuPv02X8Hx7ojMg7ncPHOdHhRd60UpWcJqbxcMrdZqSkgJpHPN3dZkuqrKjpKtxJAgRVDU6TimBNYlcz6mLYr36bv4WbFuv6W04WfC5e9IPonSjGPY1sYZKNAw7EJGOpNJ7e_GbHsC7LzNX4ybBK4WZ9Vg1_T5St_A',
     "Content-Type": "application/json"
     }
 
@@ -68,9 +94,18 @@ def genre():
         "limit": 3600
     }
 
-    d = requests.get("https://api.spotify.com/v1/recommendations?limit=2&seed_genres=classical%2Ccountry&seed_tracks=0c6xIDDpzE81m2q797ordA", params=user_params, headers=user_headers)
-    print(d)
-    for song in d["tracks"]:
+    genre_string = ""
+    print(genres)
+    for i in genres:
+
+        genre_string = genre_string + i + "%2c"
+    genre_string = genre_string[0:len(genre_string)-3]
+    print("here",genre_string)
+    d = requests.get("https://api.spotify.com/v1/recommendations?limit=2&seed_genres={}".format(genre_string), params=user_params, headers=user_headers)
+    print(d.json())
+
+
+    for song in d.json()["tracks"]:
         print(song["name"])
         #playlist.append(song["id"])
         ids.append(song['id'])
