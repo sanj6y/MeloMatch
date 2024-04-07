75198da5762c7581d3e11dd8cb9f7507a6f81981
bhavan060 <70976789+bhavan060@users.noreply.github.com>
2024-04-06 19:02:33 -0700

    finished landing page

--- src/LoadingPage.jsx
+++ src/LoadingPage.jsx
@@ -4,12 +4,25 @@ import React from "react";
 import NavBar from "./NavBar.jsx"
 import './LoadingPage.css'
 // const backgroundImage1 = require("listeningtomusic.webp")
+// import Typewriter from 'typewriter-effect/dist/core';
+import {TypeAnimation} from 'react-type-animation';
+
+const logo = require("./listen.png")
+
 
 function LoadingPage() {
   return (
-    <div>
+    <div className="landingHolder">
       <NavBar/> 
-      <div className="background-image1">
+      <div className="background-image1" >
+        <div className="welcome-text">
+          <p className="dawg1"><TypeAnimation cursor={true} sequence={['Having trouble finding new music?', 1000, 'Tired of listening to the same songs over and over?', 1000]} repeat={Infinity} speed={50}wrapper="span" className="special" /></p>
+          <p className="dawg2">MeloMatch is the website for you.</p>
+          <button className="getStarted-button">
+            Get Started
+          </button>
+        </div>
+        
       </div>
     </div>
 
--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,11 +1,28 @@
 import React from "react";
-
-
+import LogIn from "./LogIn";
 const logo = require('./logo.png');
 
+
 function NavBar(){
+
+    const CLIENT_ID = '05345fe6718845e98a33b228f41eb512'
+    const SPOTIFY_AUTH_ENDPT = 'https://accounts.spotify.com/authorize'
+    const REDIRECT_URL = 'http://localhost:3000/main'
+    const SCOPES = ['playlist-read-private', 'playlist-read-collaborative', 'user-library-read', 'playlist-modify-public',
+        'playlist-modify-private']
+    const SCOPES_URL_PARAM = SCOPES.join("%20")
+
+    const requestAuthorization = () => {
+        console.log("button pressed");
+        window.location = `${SPOTIFY_AUTH_ENDPT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URL}&scope=${SCOPES_URL_PARAM}&response_type=token&show_dialog=true`;
+    }
+
+    const redirect = (direction) => {
+      window.location.href = "#" + direction;
+    }
+    
     return (
-        <div>
+        <div className="NavbarHolder">
         <nav class="bg-white dark:bg-white fixed w-full z-20 top-0 start-0">
           <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <a href="https://google.com/" target="_blank" class="flex items-center space-x-3 rtl:space-x-reverse">
@@ -13,7 +30,7 @@ function NavBar(){
               <span class="self-center text-4xl font-thin font-semibold whitespace-nowrap text-orange-400">Melo<span class="self-center text-4xl font-semibold whitespace-nowrap text-orange-500">Match</span></span>
           </a>
           <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-              <button type="button" class=" text-[17px]  text-white bg-orange-900 hover:bg-orange-800 focus:ring-4 focus:outline-none focus:ring-orange-300 font-medium rounded-lg text-base px-4 py-2 text-center dark:bg-purple-600 dark:hover:bg-orange-700 dark:focus:ring-orange-800" >Get Started</button>
+              <button type="button" onClick={requestAuthorization} class=" text-[17px]  text-white bg-orange-900 hover:bg-orange-800 focus:ring-4 focus:outline-none focus:ring-orange-300 font-medium rounded-lg text-base px-4 py-2 text-center dark:bg-purple-600 dark:hover:bg-orange-700 dark:focus:ring-orange-800">Get Started</button>
               <button data-collapse-toggle="navbar-sticky" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-base text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
             </button>
           </div>
@@ -23,13 +40,13 @@ function NavBar(){
               <a href="#" class="block py-2 px-3 text-gray-500 rounded hover:bg-gray-800 md:hover:bg-transparent md:hover:text-gray-400 md:p-0 md:dark:hover:text-white-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Home</a>
               </li>
               <li>
-                <a href="#" class="block py-2 px-3 text-gray-500 rounded hover:bg-gray-800 md:hover:bg-transparent md:hover:text-gray-400 md:p-0 md:dark:hover:text-white-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>
+                <a href="#about" class="block py-2 px-3 text-gray-500 rounded hover:bg-gray-800 md:hover:bg-transparent md:hover:text-gray-400 md:p-0 md:dark:hover:text-white-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>
               </li>
               <li>
-                <a href="#" class="block py-2 px-3 text-gray-500 rounded hover:bg-gray-800 md:hover:bg-transparent md:hover:text-gray-400 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Services</a>
+                <a href="#services" class="block py-2 px-3 text-gray-500 rounded hover:bg-gray-800 md:hover:bg-transparent md:hover:text-gray-400 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Services</a>
               </li>
               <li>
-                <a href="#" class="block py-2 px-3 text-gray-500 rounded hover:bg-gray-800 md:hover:bg-transparent md:hover:text-gray-400 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
+                <a href="#contact" class="block py-2 px-3 text-gray-500 rounded hover:bg-gray-800 md:hover:bg-transparent md:hover:text-gray-400 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
               </li>
             </ul>
           </div>
