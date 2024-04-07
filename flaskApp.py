f7d7667b95fd81687d7fd29f47ab98cc766fae14
Sanjay Chandrasekar <sanjay.chandrasekar@gmail.com>
2024-04-06 21:31:15 -0700

    m

--- flaskApp.py
+++ flaskApp.py
@@ -1,10 +1,15 @@
 from flask import Flask, request, jsonify, Response
 from flask_cors import CORS, cross_origin
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id='05345fe6718845e98a33b228f41eb512',
+    client_secret='f15f734ac86e4151a7ee6e176e821300'))
 
 app = Flask(__name__)
 # app.config['CORS_HEADERS'] = 'Content-Type'
 CORS(app)
 
+playlist = []
 
 @app.route('/')
 def hello():
@@ -17,13 +22,19 @@ def get_string():
     print(request.data)
     return {"response": "ok"}
 
-@app.route('/post_string', methods=['POST'])
+@app.route('/song_rec', methods=['POST'])
 @cross_origin(origin='localhost',headers=['Content- Type','Authorization'])
-def post_string():
+def song_rec():
     # Logic to receive the string from the frontend
-    data = request.get_data()
-    print(data)
+    global playlist
+    id = request.get_data()
     
+    d = sp.recommendations(seed_tracks=[str(id)], limit=2)
+
+    for song in d["tracks"]:
+        print(song["name"])
+        playlist.append(song["id"])
+
     # Logic to process the string
     
     return jsonify({'message': 'String received and processed successfully'})
